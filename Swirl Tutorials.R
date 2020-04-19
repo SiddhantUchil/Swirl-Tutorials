5882fc1c366988f9aabd67dd9cc3298517abb329
Siddhant Uchil <siddhant.uchil@somaiya.edu>
2020-04-19 19:42:51 +0530

    Lesson7

--- Swirl Tutorials.R
+++ Swirl Tutorials.R
@@ -125,3 +125,28 @@ names(vect2) <- c("foo", "bar", "norf")
 identical(vect, vect21)
 vect["bar"]
 vect[c("foo", "bar")]
+
+my_vector <- c(1:20)
+my_vector <- 1:20
+my_vector
+dim(my_vector)
+length(my_vector)
+dim(my_vector) <- c(4,5)
+dim(my_vector)
+attributes(my_vector)
+my_vector
+class(my_vector)
+my_matrix <- my_vector
+?matrix
+my_matrix2 <- matrix(1:20, 4, 5)
+identical(my_matrix, my_matrix2)
+patients <- c("Bill", "Gina", "Kelly", "Sean")
+cbind(patients, my_matrix)
+my_data <- data.frame(patients, my_matrix)
+my_data
+class(my_data)
+cnames <- c("patient", "age", "weight", "bp", "rating", "test")
+colnames(my_data) <- cnames
+my_data
+bye()
+
