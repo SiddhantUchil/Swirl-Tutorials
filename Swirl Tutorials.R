41781f77333276f65b107fc94596aff3541da592
Siddhant Uchil <siddhant.uchil@somaiya.edu>
2020-04-19 18:48:06 +0530

    Lesson4

--- Swirl Tutorials.R
+++ Swirl Tutorials.R
@@ -71,3 +71,27 @@ seq_along(my_seq)
 rep(0, times = 40)
 rep(c(0, 1, 2), times = 10)
 rep(c(0, 1, 2), each = 10)
+swirl()
+library(swirl)
+swirl()
+Siddhant
+num_vect <- c(0.5, 55, -10, 6)
+tf <- num_vect < 1
+tf
+num_vect >= 6
+(3 > 5) & (4 == 4)
+((111 >= 111) | !(TRUE)) & ((4 + 1) == 5)
+my_char <- c("My", "name", "is")
+my_char
+paste(my_char, collapse = " ")
+my_name <- c(my_char, "Siddhant")
+my_name
+paste(my_name, collapse = " ")
+paste("Hello", "world!", sep = " ")
+paste(c(1:3), c("x", "y", "z"), sep = "")
+paste(1:3, c("X", "Y", "Z"), sep = "")
+paste(LETTERS, 1:4, sep = "-")
+x <- c(44, NA, 5, NA)
+x * 3
+y <- rnorm(1000)
+z <- rep(NA, 1000)
